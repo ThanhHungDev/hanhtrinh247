062c1bbc1af3aa97bbcb1ccf49ed365a40f04946
Hùng tt <thanhhung.code@gmail.com>
2020-07-18 11:31:59 +0900

    update

--- SERVER_PHP/routes/routing/admin.php
+++ SERVER_PHP/routes/routing/admin.php
@@ -1,79 +1,79 @@
 <?php 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'admin'], function () {
+// Route::group(['prefix' => 'admin'], function () {
 
-    Route::get('/login','Admin\LoginController@login')->name('ADMIN_LOGIN');
-    Route::post('/login','Admin\LoginController@postLogin')->name('ADMIN_POST_LOGIN');
-    Route::get('/logout','Admin\LoginController@logout')->name('ADMIN_LOGOUT');
+//     Route::get('/login','Admin\LoginController@login')->name('ADMIN_LOGIN');
+//     Route::post('/login','Admin\LoginController@postLogin')->name('ADMIN_POST_LOGIN');
+//     Route::get('/logout','Admin\LoginController@logout')->name('ADMIN_LOGOUT');
 
-    Route::group(['prefix' => '/', 'middleware' => 'ADMIN_LOGGED'], function () {
-        Route::get('/','Admin\AdminController@index')->name('ADMIN_SITTER');
-        Route::get('/detail-sitter/{id}','Admin\AdminController@ajaxDetailSitter')->name('ADMIN_SITTER_AJAX_VIEW');
-        Route::post('/update-publish-sitter','Admin\AdminController@ajaxUpdatePublishSitter')->name('ADMIN_SITTER_AJAX_UPDATE_PUBLISH');
-        Route::post('/delete-sitter','Admin\AdminController@ajaxUserDelete')->name('ADMIN_SITTER_AJAX_DELETE');
-        Route::post('/accept-sitter','Admin\AdminController@ajaxUserAccept')->name('ADMIN_SITTER_AJAX_ACCEPT');
-        Route::get('/employers','Admin\AdminController@employers')->name('ADMIN_EMPLOYERS');
-        Route::get('/chat','Admin\AdminController@chat')->name('ADMIN_CHAT');
-        Route::get('/support','Admin\AdminController@support')->name('ADMIN_SUPPORT');
-        Route::get('/message-reader/{id}/{channel?}','Admin\AdminController@messageReader')->name('ADMIN_READ_MESSAGE');
-        Route::post('/ajax-get-family','Admin\AdminController@ajaxGetFamily')->name('ADMIN_AJAX_GET_FAMILY');
-        Route::post('/delete-employer','Admin\AdminController@ajaxUserDelete')->name('ADMIN_AJAX_EMPLOYER_DELETE');
-    });
-});
-
-
+//     Route::group(['prefix' => '/', 'middleware' => 'ADMIN_LOGGED'], function () {
+//         Route::get('/','Admin\AdminController@index')->name('ADMIN_SITTER');
+//         Route::get('/detail-sitter/{id}','Admin\AdminController@ajaxDetailSitter')->name('ADMIN_SITTER_AJAX_VIEW');
+//         Route::post('/update-publish-sitter','Admin\AdminController@ajaxUpdatePublishSitter')->name('ADMIN_SITTER_AJAX_UPDATE_PUBLISH');
+//         Route::post('/delete-sitter','Admin\AdminController@ajaxUserDelete')->name('ADMIN_SITTER_AJAX_DELETE');
+//         Route::post('/accept-sitter','Admin\AdminController@ajaxUserAccept')->name('ADMIN_SITTER_AJAX_ACCEPT');
+//         Route::get('/employers','Admin\AdminController@employers')->name('ADMIN_EMPLOYERS');
+//         Route::get('/chat','Admin\AdminController@chat')->name('ADMIN_CHAT');
+//         Route::get('/support','Admin\AdminController@support')->name('ADMIN_SUPPORT');
+//         Route::get('/message-reader/{id}/{channel?}','Admin\AdminController@messageReader')->name('ADMIN_READ_MESSAGE');
+//         Route::post('/ajax-get-family','Admin\AdminController@ajaxGetFamily')->name('ADMIN_AJAX_GET_FAMILY');
+//         Route::post('/delete-employer','Admin\AdminController@ajaxUserDelete')->name('ADMIN_AJAX_EMPLOYER_DELETE');
+//     });
+// });
 
 
 
 
 
 
-Route::group(['prefix' => 'admin'], function () {
-
-    Route::get('/login','LoginController@getLogin')->name('ADMIN_LOGIN');
-    Route::post('/login','LoginController@postLogin')->name('ADMIN_POST_LOGIN');
-    Route::get('/logout','LoginController@logout')->name('ADMIN_LOGOUT');
-
-    Route::group(['prefix' => '/', 'middleware' => 'CHECK_ADMIN_LOGIN'], function () {
-
-        Route::get('/dashboard','AdminController@dashboard')->name('ADMIN_DASHBOARD');
-
-        Route::get('/post','AdminController@viewInsertPost')->name('ADMIN_INSERT_POST');
-        Route::post('/post','AdminController@insertPost')->name('ADMIN_POST_INSERT_POST');
-        Route::get('/post/{id}','AdminController@getEditPost')->name('ADMIN_GET_EDIT_POST');
-        Route::post('/post/{id}','AdminController@saveEditPost')->name('ADMIN_POST_EDIT_POST');
-        Route::get('/posts','AdminController@posts')->name('ADMIN_GET_LIST_POST');
-        Route::delete('/post/{id?}','AdminController@deletePost')->name('ADMIN_DELETE_POST');
 
 
-        Route::get('/category','AdminController@viewInsertCategory')->name('ADMIN_INSERT_CATEGORY');
-        Route::post('/category','AdminController@insertCategory')->name('ADMIN_POST_INSERT_CATEGORY');
-        Route::get('/category/{id}','AdminController@getEditCategory')->name('ADMIN_GET_EDIT_CATEGORY');
-        Route::post('/category/{id}','AdminController@saveEditCategory')->name('ADMIN_POST_EDIT_CATEGORY');
-        Route::get('/categories','AdminController@categories')->name('ADMIN_GET_LIST_CATEGORY');
-        Route::delete('/category/{id?}','AdminController@deleteCategory')->name('ADMIN_DELETE_CATEGORY');
+// Route::group(['prefix' => 'admin'], function () {
+
+//     Route::get('/login','LoginController@getLogin')->name('ADMIN_LOGIN');
+//     Route::post('/login','LoginController@postLogin')->name('ADMIN_POST_LOGIN');
+//     Route::get('/logout','LoginController@logout')->name('ADMIN_LOGOUT');
+
+//     Route::group(['prefix' => '/', 'middleware' => 'CHECK_ADMIN_LOGIN'], function () {
+
+//         Route::get('/dashboard','AdminController@dashboard')->name('ADMIN_DASHBOARD');
+
+//         Route::get('/post','AdminController@viewInsertPost')->name('ADMIN_INSERT_POST');
+//         Route::post('/post','AdminController@insertPost')->name('ADMIN_POST_INSERT_POST');
+//         Route::get('/post/{id}','AdminController@getEditPost')->name('ADMIN_GET_EDIT_POST');
+//         Route::post('/post/{id}','AdminController@saveEditPost')->name('ADMIN_POST_EDIT_POST');
+//         Route::get('/posts','AdminController@posts')->name('ADMIN_GET_LIST_POST');
+//         Route::delete('/post/{id?}','AdminController@deletePost')->name('ADMIN_DELETE_POST');
 
 
-        Route::get('/type','AdminController@viewInsertType')->name('ADMIN_INSERT_TYPE');
-        Route::post('/type','AdminController@insertType')->name('ADMIN_POST_INSERT_TYPE');
-        Route::get('/type/{id}','AdminController@getEditType')->name('ADMIN_GET_EDIT_TYPE');
-        Route::post('/type/{id}','AdminController@saveEditType')->name('ADMIN_POST_EDIT_TYPE');
-        Route::get('/types','AdminController@types')->name('ADMIN_GET_LIST_TYPE');
-        Route::delete('/type/{id?}','AdminController@deleteType')->name('ADMIN_DELETE_TYPE');
+//         Route::get('/category','AdminController@viewInsertCategory')->name('ADMIN_INSERT_CATEGORY');
+//         Route::post('/category','AdminController@insertCategory')->name('ADMIN_POST_INSERT_CATEGORY');
+//         Route::get('/category/{id}','AdminController@getEditCategory')->name('ADMIN_GET_EDIT_CATEGORY');
+//         Route::post('/category/{id}','AdminController@saveEditCategory')->name('ADMIN_POST_EDIT_CATEGORY');
+//         Route::get('/categories','AdminController@categories')->name('ADMIN_GET_LIST_CATEGORY');
+//         Route::delete('/category/{id?}','AdminController@deleteCategory')->name('ADMIN_DELETE_CATEGORY');
 
-        Route::get('/style','AdminController@viewInsertStyle')->name('ADMIN_INSERT_STYLE');
-        Route::post('/style','AdminController@insertStyle')->name('ADMIN_POST_INSERT_STYLE');
-        Route::get('/style/{id}','AdminController@getEditStyle')->name('ADMIN_GET_EDIT_STYLE');
-        Route::post('/style/{id}','AdminController@saveEditStyle')->name('ADMIN_POST_EDIT_STYLE');
-        Route::get('/styles','AdminController@styles')->name('ADMIN_GET_LIST_STYLE');
-        Route::delete('/style/{id?}','AdminController@deleteStyle')->name('ADMIN_DELETE_STYLE');
+
+//         Route::get('/type','AdminController@viewInsertType')->name('ADMIN_INSERT_TYPE');
+//         Route::post('/type','AdminController@insertType')->name('ADMIN_POST_INSERT_TYPE');
+//         Route::get('/type/{id}','AdminController@getEditType')->name('ADMIN_GET_EDIT_TYPE');
+//         Route::post('/type/{id}','AdminController@saveEditType')->name('ADMIN_POST_EDIT_TYPE');
+//         Route::get('/types','AdminController@types')->name('ADMIN_GET_LIST_TYPE');
+//         Route::delete('/type/{id?}','AdminController@deleteType')->name('ADMIN_DELETE_TYPE');
+
+//         Route::get('/style','AdminController@viewInsertStyle')->name('ADMIN_INSERT_STYLE');
+//         Route::post('/style','AdminController@insertStyle')->name('ADMIN_POST_INSERT_STYLE');
+//         Route::get('/style/{id}','AdminController@getEditStyle')->name('ADMIN_GET_EDIT_STYLE');
+//         Route::post('/style/{id}','AdminController@saveEditStyle')->name('ADMIN_POST_EDIT_STYLE');
+//         Route::get('/styles','AdminController@styles')->name('ADMIN_GET_LIST_STYLE');
+//         Route::delete('/style/{id?}','AdminController@deleteStyle')->name('ADMIN_DELETE_STYLE');
         
 
-        Route::get('/slug/{slug?}','AdminController@slug')->name('ADMIN_GET_SLUG');
-        Route::get('/typesByCategory', 'AdminController@categoryTypesByCategoryId')->name('ADMIN_GET_TYPE_BY_CATEGORY');
-        Route::get('/stylesByCategory', 'AdminController@categoryStylesByCategoryId')->name('ADMIN_GET_STYLE_BY_CATEGORY');
-        Route::get('/categoryByType', 'AdminController@categoryByTypeId')->name('ADMIN_GET_CATEGORY_BY_TYPE');
-        Route::get('/stylesByType', 'AdminController@categoryStylesByTypeId')->name('ADMIN_GET_STYLE_BY_TYPE');
-    });
-});
+//         Route::get('/slug/{slug?}','AdminController@slug')->name('ADMIN_GET_SLUG');
+//         Route::get('/typesByCategory', 'AdminController@categoryTypesByCategoryId')->name('ADMIN_GET_TYPE_BY_CATEGORY');
+//         Route::get('/stylesByCategory', 'AdminController@categoryStylesByCategoryId')->name('ADMIN_GET_STYLE_BY_CATEGORY');
+//         Route::get('/categoryByType', 'AdminController@categoryByTypeId')->name('ADMIN_GET_CATEGORY_BY_TYPE');
+//         Route::get('/stylesByType', 'AdminController@categoryStylesByTypeId')->name('ADMIN_GET_STYLE_BY_TYPE');
+//     });
+// });
--- SERVER_PHP/routes/routing/client.php
+++ SERVER_PHP/routes/routing/client.php
@@ -5,66 +5,68 @@ Route::get('/404', function(){
     echo 'ページが見つかりません';
 })->name('CLIENT_404');
 
-Route::get('/','Client\TopPageController@index')->name('TOP_PAGE');
-Route::get('/search','Client\SearchController@searchSitters')->name('search-sitters');
-Route::get('/ajax-load-items','Client\TopPageController@ajaxLoadItems')->name('ajax-load-items');
-Route::get('/advance-search','Client\SearchController@advanceSearch')->name('advance-search');
-Route::get('/calendar','Client\TopPageController@calendar')->name('calendar');
-Route::get('/calendar-view','Client\TopPageController@calendarViewer')->name('calendar_view');
-Route::get('/view-sitter/{id}','Client\TopPageController@viewSitter')->name('VIEW_SITTER');
+Route::get('/', function(){
+    return " hùng đẹp trai ";
+})->name('TOP_PAGE');
+// Route::get('/search','Client\SearchController@searchSitters')->name('search-sitters');
+// Route::get('/ajax-load-items','Client\TopPageController@ajaxLoadItems')->name('ajax-load-items');
+// Route::get('/advance-search','Client\SearchController@advanceSearch')->name('advance-search');
+// Route::get('/calendar','Client\TopPageController@calendar')->name('calendar');
+// Route::get('/calendar-view','Client\TopPageController@calendarViewer')->name('calendar_view');
+// Route::get('/view-sitter/{id}','Client\TopPageController@viewSitter')->name('VIEW_SITTER');
 
-Route::group(['prefix' => 'sitter'], function() {
-    Route::get('/login','Client\Sitter\AuthController@login')->name('SITTER_LOGIN');
-    Route::post('/login','Client\Sitter\AuthController@postLogin')->name('SITTER_POST_LOGIN');
-    Route::get('/logout','Client\Sitter\AuthController@logout')->name('SITTER_LOGOUT');
-    Route::get('/forgot','Client\Sitter\AuthController@forgot')->name('SITTER_FORGOT');
-    Route::post('/forgot','Client\Sitter\AuthController@postForgot')->name('SITTER_POST_FORGOT');
-    Route::get('/register-comfirm','Client\Sitter\AuthController@registerComfirm')->name('SITTER_REGISTER_COMFIRM');
-    Route::get('/renew-password','Client\Sitter\AuthController@renewPassword')->name('SITTER_RENEW_PASSWORD');
-    Route::post('/renew-password','Client\Sitter\AuthController@postRenewPassword')->name('SITTER_POST_RENEW_PASSWORD');
-    Route::get('/register','Client\Sitter\RegisterController@register')->name('SITTER_REGISTER');
-    Route::post('/register','Client\Sitter\RegisterController@postRegister')->name('SITTER_POST_REGISTER');
-    Route::get('/register-success','Client\Sitter\RegisterController@registerSuccess')->name('SITTER_REGISTER_SUCCESS');
-    Route::get('/register-parent','Client\Sitter\RegisterController@registerParent')->name('SITTER_REGISTER_NEXT_STEP');
-    Route::post('/register-parent','Client\Sitter\RegisterController@postRegisterParent')->name('SITTER_POST_REGISTER_NEXT_STEP');
+// Route::group(['prefix' => 'sitter'], function() {
+//     Route::get('/login','Client\Sitter\AuthController@login')->name('SITTER_LOGIN');
+//     Route::post('/login','Client\Sitter\AuthController@postLogin')->name('SITTER_POST_LOGIN');
+//     Route::get('/logout','Client\Sitter\AuthController@logout')->name('SITTER_LOGOUT');
+//     Route::get('/forgot','Client\Sitter\AuthController@forgot')->name('SITTER_FORGOT');
+//     Route::post('/forgot','Client\Sitter\AuthController@postForgot')->name('SITTER_POST_FORGOT');
+//     Route::get('/register-comfirm','Client\Sitter\AuthController@registerComfirm')->name('SITTER_REGISTER_COMFIRM');
+//     Route::get('/renew-password','Client\Sitter\AuthController@renewPassword')->name('SITTER_RENEW_PASSWORD');
+//     Route::post('/renew-password','Client\Sitter\AuthController@postRenewPassword')->name('SITTER_POST_RENEW_PASSWORD');
+//     Route::get('/register','Client\Sitter\RegisterController@register')->name('SITTER_REGISTER');
+//     Route::post('/register','Client\Sitter\RegisterController@postRegister')->name('SITTER_POST_REGISTER');
+//     Route::get('/register-success','Client\Sitter\RegisterController@registerSuccess')->name('SITTER_REGISTER_SUCCESS');
+//     Route::get('/register-parent','Client\Sitter\RegisterController@registerParent')->name('SITTER_REGISTER_NEXT_STEP');
+//     Route::post('/register-parent','Client\Sitter\RegisterController@postRegisterParent')->name('SITTER_POST_REGISTER_NEXT_STEP');
 
-    Route::group(['middleware' => 'SITTER_LOGGED'], function() {
-        Route::get('/chat/{id?}','Client\Sitter\SitterController@chat')->name('SITTER_CHAT');
-        Route::get('/','Client\Sitter\SitterController@index')->name('SITTER_MYPAGE');
-        Route::get('/edit','Client\Sitter\SitterController@editMypage')->name('SITTER_EDIT');
-        Route::post('/edit','Client\Sitter\SitterController@postEditMypage')->name('SITTER_POST_EDIT');
-        Route::post('/remove-avatar','Client\Sitter\SitterController@ajaxRemoveAvatar')->name('SITTER_AJAX_REMOVE_AVATAR');
-        Route::post('/upload-avatar','Client\Sitter\SitterController@ajaxUploadAvatar')->name('SITTER_AJAX_ADD_AVATAR');
-        Route::get('/load-avatars','Client\Sitter\SitterController@ajaxLoadAvatars')->name('SITTER_AJAX_LOAD_AVATAR');
-        Route::get('/publish-profile','Client\Sitter\SitterController@ajaxPublishProfile')->name('SITTER_AJAX_PUBLISH_PROFILE');
-    });
-});
+//     Route::group(['middleware' => 'SITTER_LOGGED'], function() {
+//         Route::get('/chat/{id?}','Client\Sitter\SitterController@chat')->name('SITTER_CHAT');
+//         Route::get('/','Client\Sitter\SitterController@index')->name('SITTER_MYPAGE');
+//         Route::get('/edit','Client\Sitter\SitterController@editMypage')->name('SITTER_EDIT');
+//         Route::post('/edit','Client\Sitter\SitterController@postEditMypage')->name('SITTER_POST_EDIT');
+//         Route::post('/remove-avatar','Client\Sitter\SitterController@ajaxRemoveAvatar')->name('SITTER_AJAX_REMOVE_AVATAR');
+//         Route::post('/upload-avatar','Client\Sitter\SitterController@ajaxUploadAvatar')->name('SITTER_AJAX_ADD_AVATAR');
+//         Route::get('/load-avatars','Client\Sitter\SitterController@ajaxLoadAvatars')->name('SITTER_AJAX_LOAD_AVATAR');
+//         Route::get('/publish-profile','Client\Sitter\SitterController@ajaxPublishProfile')->name('SITTER_AJAX_PUBLISH_PROFILE');
+//     });
+// });
 
-Route::group(['prefix' => 'employer'], function() {
+// Route::group(['prefix' => 'employer'], function() {
 
-    Route::get('/register','Client\employer\RegisterController@register')->name('EMPLOYER_REGISTER');
-    Route::post('/register','Client\employer\RegisterController@store')->name('EMPLOYER_POST_REGISTER');
-    Route::get('/register-comfirm','Client\employer\RegisterController@registerComfirm')->name('EMPLOYER_REGISTER_COMFIRM');
-    Route::get('/register-homie','Client\employer\RegisterController@registerParent')->name('EMPLOYER_REGISTER_PARENT');
-    Route::post('/register-homie','Client\employer\RegisterController@postRegisterParent')->name('EMPLOYER_POST_REGISTER_PARENT');
-    Route::get('/register-success','Client\employer\RegisterController@registerSuccess')->name('EMPLOYER_REGISTER_SUCCESS');
-    Route::get('/login/{route?}/{param?}','Client\employer\LoginController@login')->name('EMPLOYER_LOGIN');
-    Route::post('/login','Client\employer\LoginController@postLogin')->name('EMPLOYER_POST_LOGIN');
-    Route::get('/logout','Client\employer\LoginController@logout')->name('EMPLOYER_LOGOUT');
-    Route::get('/forgot','Client\employer\RegisterController@forgot')->name('EMPLOYER_FORGOT');
-    Route::post('/forgot','Client\employer\RegisterController@postForgot')->name('EMPLOYER_POST_FORGOT');
-    Route::get('/renew-password','Client\employer\RegisterController@renewPassword')->name('EMPLOYER_RENEW_PASSWORD');
-    Route::post('/renew-password','Client\employer\RegisterController@postRenewPassword')->name('EMPLOYER_POST_RENEW_PASSWORD');
+//     Route::get('/register','Client\employer\RegisterController@register')->name('EMPLOYER_REGISTER');
+//     Route::post('/register','Client\employer\RegisterController@store')->name('EMPLOYER_POST_REGISTER');
+//     Route::get('/register-comfirm','Client\employer\RegisterController@registerComfirm')->name('EMPLOYER_REGISTER_COMFIRM');
+//     Route::get('/register-homie','Client\employer\RegisterController@registerParent')->name('EMPLOYER_REGISTER_PARENT');
+//     Route::post('/register-homie','Client\employer\RegisterController@postRegisterParent')->name('EMPLOYER_POST_REGISTER_PARENT');
+//     Route::get('/register-success','Client\employer\RegisterController@registerSuccess')->name('EMPLOYER_REGISTER_SUCCESS');
+//     Route::get('/login/{route?}/{param?}','Client\employer\LoginController@login')->name('EMPLOYER_LOGIN');
+//     Route::post('/login','Client\employer\LoginController@postLogin')->name('EMPLOYER_POST_LOGIN');
+//     Route::get('/logout','Client\employer\LoginController@logout')->name('EMPLOYER_LOGOUT');
+//     Route::get('/forgot','Client\employer\RegisterController@forgot')->name('EMPLOYER_FORGOT');
+//     Route::post('/forgot','Client\employer\RegisterController@postForgot')->name('EMPLOYER_POST_FORGOT');
+//     Route::get('/renew-password','Client\employer\RegisterController@renewPassword')->name('EMPLOYER_RENEW_PASSWORD');
+//     Route::post('/renew-password','Client\employer\RegisterController@postRenewPassword')->name('EMPLOYER_POST_RENEW_PASSWORD');
     
-    Route::group(['middleware' => 'EMPLOYER_LOGGED'], function() {
-        Route::get('/','Client\employer\EmployerController@myPage')->name('EMPLOYER_MYPAGE');
-        Route::get('/chat/{id?}','Client\employer\EmployerController@chat')->name('EMPLOYER_CHAT');
-        Route::get('/edit','Client\employer\EmployerController@editProfile')->name('EMPLOYER_EDIT_PROFILE');
-        Route::post('/edit','Client\employer\EmployerController@postEditProfile')->name('EMPLOYER_POST_EDIT_PROFILE');
-        Route::post('/ajax-delete-member','Client\employer\EmployerController@ajaxDeleteMember')->name('AJAX_DELETE_MEMBER_FAMILY');
-        Route::post('/upload-avatar','Client\employer\EmployerController@ajaxUploadAvatar')->name('AJAX_UPLOAD_AVATAR');
-    });
-});
+//     Route::group(['middleware' => 'EMPLOYER_LOGGED'], function() {
+//         Route::get('/','Client\employer\EmployerController@myPage')->name('EMPLOYER_MYPAGE');
+//         Route::get('/chat/{id?}','Client\employer\EmployerController@chat')->name('EMPLOYER_CHAT');
+//         Route::get('/edit','Client\employer\EmployerController@editProfile')->name('EMPLOYER_EDIT_PROFILE');
+//         Route::post('/edit','Client\employer\EmployerController@postEditProfile')->name('EMPLOYER_POST_EDIT_PROFILE');
+//         Route::post('/ajax-delete-member','Client\employer\EmployerController@ajaxDeleteMember')->name('AJAX_DELETE_MEMBER_FAMILY');
+//         Route::post('/upload-avatar','Client\employer\EmployerController@ajaxUploadAvatar')->name('AJAX_UPLOAD_AVATAR');
+//     });
+// });
 
 
-Route::get('/admin','Admin\AdminController@index')->name('ADMIN-INDEX');
+// Route::get('/admin','Admin\AdminController@index')->name('ADMIN-INDEX');
