339fe3909bd0223e7cb5c4f828ecaf9fe7da4bac
Trương Thanh hùng <thuong_thanh_hung@managerment-partners.co.jp>
2020-09-01 09:37:51 +0900

    f

--- EX/index.js
+++ EX/index.js
@@ -1,16 +1,42 @@
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require('express');
-const path = require('path');
+
 const app = express();
 
 // Allow dotfiles - this is required for verification by Lets Encrypt's certbot
 // app.use(express.static(path.join(__dirname, 'build'), {dotfiles: 'allow'}));
 app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/chat.ebudezain.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/chat.ebudezain.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/chat.ebudezain.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-console.log(path.join(__dirname, 'index.html'))
-app.listen(3000);
+
+// Starting both http & https servers
+// const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+// httpServer.listen(80, () => {
+// 	console.log('HTTP Server running on port 80');
+// });
+
+httpsServer.listen(3000, () => {
+	console.log('HTTPS Server running on port 443');
+});
 
 
 
