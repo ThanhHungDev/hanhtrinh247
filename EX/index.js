4af1fd14f914c182204c68e38d3cdf149658f74e
Trương Thanh hùng <thuong_thanh_hung@managerment-partners.co.jp>
2020-09-01 09:24:57 +0900

    update

--- EX/index.js
+++ EX/index.js
@@ -3,12 +3,13 @@ const path = require('path');
 const app = express();
 
 // Allow dotfiles - this is required for verification by Lets Encrypt's certbot
-app.use(express.static(path.join(__dirname, 'build'), {dotfiles: 'allow'}));
+// app.use(express.static(path.join(__dirname, 'build'), {dotfiles: 'allow'}));
+app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
-console.log(path.join(__dirname, 'build', 'index.html'))
+console.log(path.join(__dirname, 'index.html'))
 app.listen(3000);
 
 
